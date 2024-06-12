aa49271044a2be08f55b189f38447b2d8e5c3a70
Kevin Tanuwijaya <kevintanuwijayasao@gmail.com>
2024-06-12 15:05:17 +0700

    setup pipe, cors & swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,56 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    logger: ['error', 'log', 'debug'],
+  });
+
+  // CORS
+  const options: CorsOptions = {
+    origin: '*',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  };
+  app.enableCors(options);
+
+  // Validation Pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription(
+      'The API Documentation provides developers with clear instructions and endpoints for integrating with both the web and mobile applications. Access user information, manage content, and facilitate interactions within the MMA community with ease. Simplify development with straightforward examples and detailed parameters.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'Authorization',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const PORT = process.env.PORT || 2000;
+  await app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
 }
 bootstrap();
